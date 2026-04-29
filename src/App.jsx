7922cbdc8f28af3192e66a6fa48081908d9ae324
Edwin <ewn.francisco@gmail.com>
2026-04-29 20:19:54 -0300

    chore: configura react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Banner from './components/Banner'
 import Header from './components/Header'
-import { GlobalStyle, Container } from './styles'
+import { GlobalStyle } from './styles'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Banner />,
+  },
+])
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Container>
+      <div className="container">
         <Header />
-      </Container>
+      </div>
+      <RouterProvider router={rotas} />
     </>
   )
 }
