e89507123995ffdb085198566b081b54aa37dd54
Edwin <ewn.francisco@gmail.com>
2026-05-31 13:32:48 -0300

    feat: desenvolve lógica para adicionar e remover itens no carrinho com react-redux

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,3 +1,6 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+
 import Button from '../Button'
 import Tag from '../Tag'
 import {
@@ -9,41 +12,57 @@ import {
   CartItem,
 } from './styles'
 
-const Cart = () => (
-  <CartContainer>
-    <Overlay />
-    <SideBar>
-      <ul>
-        <CartItem>
-          <img src="https://placehold.co/80x80" alt="Nome do jogo" />
-          <div>
-            <h3>Nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 235,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-        <CartItem>
-          <img src="https://placehold.co/80x80" alt="Nome do jogo" />
-          <div>
-            <h3>Nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 235,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-      </ul>
-      <Quantity>2 jogo(s) no carrinho</Quantity>
-      <Prices>
-        Total: R$ 235,00<span>Em até 6x sem juros</span>
-      </Prices>
-      <Button title="Continuar com a compra" type="button">
-        Continuar com a compra
-      </Button>
-    </SideBar>
-  </CartContainer>
-)
+import { close, remove } from '../../store/reducers/cart'
+import { priceFormatter } from '../ProductsList'
+
+const Cart = () => {
+  const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
+
+  const dispatch = useDispatch()
+
+  const closeCart = () => {
+    dispatch(close())
+  }
+
+  const getTotalPrice = () => {
+    return items.reduce((total, item) => {
+      return (total += item.prices.current!)
+    }, 0)
+  }
+
+  const removeItem = (id: number) => {
+    dispatch(remove(id))
+  }
+
+  return (
+    <CartContainer className={isOpen ? 'is-open' : ''}>
+      <Overlay onClick={closeCart} />
+      <SideBar>
+        <ul>
+          {items.map((item) => (
+            <CartItem key={item.id}>
+              <img src={item.media.cover} alt={item.name} />
+              <div>
+                <h3>{item.name}</h3>
+                <Tag>{item.details.category}</Tag>
+                <Tag>{item.details.system}</Tag>
+                <span>{priceFormatter(item.prices.current)}</span>
+              </div>
+              <button onClick={() => removeItem(item.id)} type="button" />
+            </CartItem>
+          ))}
+        </ul>
+        <Quantity>{items.length} jogo(s) no carrinho</Quantity>
+        <Prices>
+          Total: {priceFormatter(getTotalPrice())}
+          <span>Em até 6x sem juros</span>
+        </Prices>
+        <Button title="Continuar com a compra" type="button">
+          Continuar com a compra
+        </Button>
+      </SideBar>
+    </CartContainer>
+  )
+}
 
 export default Cart
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,40 +5,53 @@ import Tag from '../Tag'
 import { priceFormatter } from '../ProductsList'
 
 import { Banner, Infos } from './styles'
+import { useDispatch } from 'react-redux'
+
+import { add, open } from '../../store/reducers/cart'
 
 type Props = {
   game: Game
 }
 
-const Hero = ({ game }: Props) => (
-  <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
-    <div className="container">
-      <div>
-        <Tag>{game.details.category}</Tag>
-        <Tag>{game.details.system}</Tag>
-      </div>
-      <Infos>
-        <h2>{game.name}</h2>
-        <p>
-          {game.prices.discount && (
-            <span>De {priceFormatter(game.prices.old)}</span>
-          )}
+const Hero = ({ game }: Props) => {
+  const dispatch = useDispatch()
+
+  const addToCart = () => {
+    dispatch(add(game))
+    dispatch(open())
+  }
+
+  return (
+    <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
+      <div className="container">
+        <div>
+          <Tag>{game.details.category}</Tag>
+          <Tag>{game.details.system}</Tag>
+        </div>
+        <Infos>
+          <h2>{game.name}</h2>
+          <p>
+            {game.prices.discount && (
+              <span>De {priceFormatter(game.prices.old)}</span>
+            )}
+            {game.prices.current && (
+              <>Por {priceFormatter(game.prices.current)}</>
+            )}
+          </p>
           {game.prices.current && (
-            <>Por {priceFormatter(game.prices.current)}</>
+            <Button
+              type="button"
+              title="Clique aqui para adicionar ao carrinho"
+              variant="primary"
+              onClick={addToCart}
+            >
+              Adicionar ao carrinho
+            </Button>
           )}
-        </p>
-        {game.prices.current && (
-          <Button
-            type="button"
-            title="Clique aqui para adicionar ao carrinho"
-            variant="primary"
-          >
-            Adicionar ao carrinho
-          </Button>
-        )}
-      </Infos>
-    </div>
-  </Banner>
-)
+        </Infos>
+      </div>
+    </Banner>
+  )
+}
 
 export default Hero
