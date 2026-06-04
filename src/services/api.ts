183e63470c49299c3e3f2156357763ed21043f50
Edwin <ewn.francisco@gmail.com>
2026-06-04 10:28:45 -0300

    feat: integração da api na página de checkout

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,40 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Game } from '../pages/Home'
 
+type Product = {
+  id: number
+  price: number
+}
+
+type PurchasePayload = {
+  products: Product[]
+  billing: {
+    name: string
+    email: string
+    document: string
+  }
+  delivery: {
+    email: string
+  }
+  payment: {
+    card: {
+      active: boolean
+      owner?: {
+        name: string
+        document: string
+      }
+      name?: string
+      number?: string
+      expires?: {
+        month: number
+        year: number
+      }
+      code?: number
+    }
+    installments: number
+  }
+}
+
 const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api-ebac.vercel.app/api/eplay',
@@ -33,6 +67,13 @@ const api = createApi({
     getGameById: builder.query<Game, string>({
       query: (id) => `jogos/${id}`,
     }),
+    purchase: builder.mutation<any, PurchasePayload>({
+      query: (body) => ({
+        url: 'checkout',
+        method: 'POST',
+        body,
+      }),
+    }),
   }),
 })
 
@@ -46,6 +87,7 @@ export const {
   useGetFightingGamesQuery,
   useGetRPGGamesQuery,
   useGetGameByIdQuery,
+  usePurchaseMutation,
 } = api
 
 export default api
