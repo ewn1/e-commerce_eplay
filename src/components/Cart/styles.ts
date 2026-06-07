e64a55df0df0b7c3c0d62f6b1f204deb0f8e0093
Edwin <ewn.francisco@gmail.com>
2026-06-07 17:19:43 -0300

    fix: ajuste do tamanho do component cart no modo responsivo

--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { colors } from '../../styles'
+import { breakpoints, colors } from '../../styles'
 import { TagContainer } from '../Tag/styles'
 import { ButtonContainer } from '../Button/styles'
 import closeIcon from '../../assets/images/closeIcon.png'
@@ -44,6 +44,10 @@ export const SideBar = styled.aside`
     line-height: 22px;
     color: ${colors.white};
     text-align: center;
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    max-width: 80%;
   }
 `
 
